5ca86bac05e5d26cf63865b9d55828a4b3b36aa9
rmummadu <rmummadu@cisco.com>
2018-12-18 23:33:19 +0530

    add println statement

--- studentreg/src/main/java/com/syncon/registation/HomeController.java
+++ studentreg/src/main/java/com/syncon/registation/HomeController.java
@@ -29,6 +29,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
 		model.addAttribute("serverTime", formattedDate );
+		System.out.println("Ravi changes in home controller....");
 		return "home";
 	}
 	
