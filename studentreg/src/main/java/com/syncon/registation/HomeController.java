24effd6d45cf4d626f4446b48d0062aca9cd014e
aravindchary <aravindcanny@gmail.com>
2018-12-19 14:38:16 +0530

    changes for home controller

--- studentreg/src/main/java/com/syncon/registation/HomeController.java
+++ studentreg/src/main/java/com/syncon/registation/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		System.out.println("aravind changes.....");
+		System.out.println("aravind changes hi ra how  r u .....");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
